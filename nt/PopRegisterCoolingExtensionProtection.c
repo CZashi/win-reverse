7dde4cef92b9c369f1c496ecc445f7d62b5bc207
czashi <113217553+CZashi@users.noreply.github.com>
2023-07-02 19:56:02 +0200

    Update PopRegisterCoolingExtensionProtection.c

--- nt/PopRegisterCoolingExtensionProtection.c
+++ nt/PopRegisterCoolingExtensionProtection.c
@@ -1,3 +1,20 @@
+struct _POP_COOLING_EXTENSION
+{
+    struct _LIST_ENTRY Link;                                                //0x0
+    struct _LIST_ENTRY RequestListHead;                                     //0x10
+    struct _POP_RW_LOCK Lock;                                               //0x20
+    struct _DEVICE_OBJECT* DeviceObject;                                    //0x30
+    VOID* NotificationEntry;                                                //0x38
+    UCHAR Enabled;                                                          //0x40
+    UCHAR ActiveEngaged;                                                    //0x41
+    UCHAR ThrottleLimit;                                                    //0x42
+    UCHAR UpdatingToCurrent;                                                //0x43
+    struct _KEVENT* RemovalFlushEvent;                                      //0x48
+    struct _KEVENT* PnpFlushEvent;                                          //0x50
+    struct _THERMAL_COOLING_INTERFACE Interface;                            //0x58
+}; 
+
+
 NTSTATUS __fastcall PopRegisterCoolingExtensionProtection(_POP_COOLING_EXTENSION *CoolingExtension)
 {
   void **NotificationEntry; // r12
