254572dbf8d465fa02d78a058f79b6543b19f487
czashi <113217553+CZashi@users.noreply.github.com>
2023-09-16 19:56:14 +0200

    Create KiProcessNMI.c

---
+++ nt/KiProcessNMI.c
@@ -0,0 +1,52 @@
+char __fastcall KiProcessNMI(_KTRAP_FRAME *TrapFrame, _KEXCEPTION_FRAME *KernelExceptionFrame)
+{
+  char NmiServiceResult;
+  char CallbackHandled;
+  int IsNmiBeingServiced
+  struct _KPRCB *Prcb;
+  int Number;
+  __int64 Handled;
+  _KNMI_HANDLER_CALLBACK *NmiListHead;
+ 
+  HalPreprocessNmi(0);
+  _mm_lfence();
+  LOBYTE(IsNmiBeingServiced) = KiCheckForFreezeExecution(TrapFrame, KernelExceptionFrame);
+  if ( !IsNmiBeingServiced )
+  {
+    Prcb = KeGetCurrentPrcb();
+    Number = Prcb->Number;
+    IsNmiBeingServiced = KeInterlockedSetProcessorAffinityEx(&KiNmiInProgress, Prcb->Number);
+    if ( !IsNmiBeingServiced )
+    {
+      HalPreprocessNmi(1);
+      NmiListHead = KiNmiCallbackListHead;
+      NmiServiceResult = 0;
+      if ( !KiNmiCallbackListHead )
+        goto hal_service_nmi;
+      do 
+      {
+        LOBYTE(Handled) = NmiServiceResult;
+        CallbackHandled = (NmiListHead->NmiCallback)(NmiListHead->Context, Handled);
+        NmiListHead = NmiListHead->Next;
+        NmiServiceResult |= CallbackHandled;
+      }
+      while ( NmiListHead );
+      if ( !NmiServiceResult )
+      {
+HalServiceNmi:
+        while ( !KxTryToAcquireSpinLock(&KiNMILock) )
+        {
+          do
+            KiCheckForFreezeExecution(TrapFrame, KernelExceptionFrame);
+          while ( KiNMILock );
+        }
+        _InterlockedCompareExchange(&KiBugCheckActive, 1, 0);
+        HalHandleNMI();
+        _InterlockedCompareExchange(&KiBugCheckActive, 0, 1);
+        KxReleaseSpinLock(&KiNMILock);
+      }
+      LOBYTE(IsNmiBeingServiced) = KeInterlockedClearProcessorAffinityEx(&KiNmiInProgress, Number);
+    }
+  }
+  return IsNmiBeingServiced;
+}
