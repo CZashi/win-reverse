ea7f3c552da611b3ca085bb0d0861178dde1094c
czashi <113217553+CZashi@users.noreply.github.com>
2023-10-27 15:04:57 +0200

    Update CmpNotifyChangeKey.c

--- nt/regnotif/CmpNotifyChangeKey.c
+++ nt/regnotif/CmpNotifyChangeKey.c
@@ -1,3 +1,15 @@
+struct _CM_NOTIFY_BLOCK
+{
+    struct _LIST_ENTRY HiveList;                                            //0x0
+    struct _LIST_ENTRY PostList;                                            //0x10
+    struct _CM_KEY_CONTROL_BLOCK* KeyControlBlock;                          //0x20
+    struct _CM_KEY_BODY* KeyBody;                                           //0x28
+    ULONG Filter:30;                                                        //0x30
+    ULONG WatchTree:1;                                                      //0x30
+    ULONG NotifyPending:1;                                                  //0x30
+    struct _SECURITY_SUBJECT_CONTEXT SubjectContext;                        //0x38
+}; 
+
 _ETW_TP_EVENT_TIMER_CANCELLED *__fastcall CmpNotifyChangeKey(
         _ETW_TP_EVENT_TIMER_CANCELLED *retstr,
         _CM_KEY_BODY *KeyBody,
