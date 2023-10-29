d0d9608b4f5424b3f0ee3f351e395a382c271e9d
czashi <113217553+CZashi@users.noreply.github.com>
2023-10-29 18:42:17 +0100

    Update ExpWnfCreateProcessContext.c

--- nt/ExpWnfCreateProcessContext.c
+++ nt/ExpWnfCreateProcessContext.c
@@ -1,3 +1,18 @@
+struct _WNF_PROCESS_CONTEXT
+{
+    struct _WNF_NODE_HEADER Header;                                         //0x0
+    struct _EPROCESS* Process;                                              //0x8
+    struct _LIST_ENTRY WnfProcessesListEntry;                               //0x10
+    VOID* ImplicitScopeInstances[3];                                        //0x20
+    struct _WNF_LOCK TemporaryNamesListLock;                                //0x38
+    struct _LIST_ENTRY TemporaryNamesListHead;                              //0x40
+    struct _WNF_LOCK ProcessSubscriptionListLock;                           //0x50
+    struct _LIST_ENTRY ProcessSubscriptionListHead;                         //0x58
+    struct _WNF_LOCK DeliveryPendingListLock;                               //0x68
+    struct _LIST_ENTRY DeliveryPendingListHead;                             //0x70
+    struct _KEVENT* NotificationEvent;                                      //0x80
+}; 
+
 NTSTATUS __fastcall ExpWnfCreateProcessContext(_EPROCESS *Process, _WNF_PROCESS_CONTEXT **ProcessContext)
 {
   NTSTATUS v2; // ebx
