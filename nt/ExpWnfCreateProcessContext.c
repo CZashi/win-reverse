d85102e2960a1c28cca63ea4435e4d32e1179396
czashi <113217553+CZashi@users.noreply.github.com>
2023-11-06 18:13:55 +0100

    Update ExpWnfCreateProcessContext.c

--- nt/ExpWnfCreateProcessContext.c
+++ nt/ExpWnfCreateProcessContext.c
@@ -18,7 +18,7 @@ NTSTATUS __fastcall ExpWnfCreateProcessContext(_EPROCESS *Process, _WNF_PROCESS_
   NTSTATUS v2; // ebx
   NTSTATUS v5; // er14
   UINT8 *PoolWithTag; // rax
-  _WNF_PROCESS_CONTEXT *v7; // rdi
+  _WNF_PROCESS_CONTEXT *pWnfProcessContext; // rdi
   _BYTE *v8; // rax
   _BYTE *v9; // rsi
   void **v10; // rcx
@@ -28,19 +28,20 @@ NTSTATUS __fastcall ExpWnfCreateProcessContext(_EPROCESS *Process, _WNF_PROCESS_
   v2 = 0;
   v5 = 0;
   PoolWithTag = (UINT8 *)ExAllocatePoolWithTag(PagedPool, 0x88u, 0x20666E57u);
-  v7 = (_WNF_PROCESS_CONTEXT *)PoolWithTag;
+  pWnfProcessContext = (_WNF_PROCESS_CONTEXT *)PoolWithTag;
   if ( PoolWithTag )
   {
     memset(PoolWithTag, 0, 0x88ui64);
-    v7->Header = (_WNF_NODE_HEADER)8915206;
-    v7->TemporaryNamesListHead.Blink = &v7->TemporaryNamesListHead;
-    v7->TemporaryNamesListHead.Flink = &v7->TemporaryNamesListHead;
-    v7->NotificationEvent = 0i64;
-    v7->ProcessSubscriptionListHead.Blink = &v7->ProcessSubscriptionListHead;
-    v7->ProcessSubscriptionListHead.Flink = &v7->ProcessSubscriptionListHead;
-    v7->Process = Process;
-    v7->DeliveryPendingListHead.Blink = &v7->DeliveryPendingListHead;
-    v7->DeliveryPendingListHead.Flink = &v7->DeliveryPendingListHead;
+    pWnfProcessContext->Header = (_WNF_NODE_HEADER)8915206;
+    pWnfProcessContext->TemporaryNamesListHead.Blink = &pWnfProcessContext->TemporaryNamesListHead;
+    pWnfProcessContext->TemporaryNamesListHead.Flink = &pWnfProcessContext->TemporaryNamesListHead;
+    pWnfProcessContext->NotificationEvent = 0i64;
+    pWnfProcessContext->ProcessSubscriptionListHead.Blink = &pWnfProcessContext->ProcessSubscriptionListHead;
+    pWnfProcessContext->ProcessSubscriptionListHead.Flink = &pWnfProcessContext->ProcessSubscriptionListHead;
+    pWnfProcessContext->Process = Process;
+    pWnfProcessContext->DeliveryPendingListHead.Blink = &pWnfProcessContext->DeliveryPendingListHead;
+    pWnfProcessContext->DeliveryPendingListHead.Flink = &pWnfProcessContext->DeliveryPendingListHead;
+      
     v8 = KeAbPreAcquire(&ExpWnfProcessesListLock, 0i64, 0i64);
     v9 = v8;
     if ( _interlockedbittestandset64((volatile signed __int32 *)&ExpWnfProcessesListLock, 0i64) )
@@ -48,13 +49,15 @@ NTSTATUS __fastcall ExpWnfCreateProcessContext(_EPROCESS *Process, _WNF_PROCESS_
     if ( v9 )
       v9[26] |= 1u;
     v10 = off_140D2C648;
-    p_WnfProcessesListEntry = &v7->WnfProcessesListEntry;
+    p_WnfProcessesListEntry = &pWnfProcessContext->WnfProcessesListEntry;
     if ( *off_140D2C648 != &ExpWnfProcessesListHead )
       __fastfail(3u);
+      
     p_WnfProcessesListEntry->Flink = (_LIST_ENTRY *)&ExpWnfProcessesListHead;
-    v7->WnfProcessesListEntry.Blink = (_LIST_ENTRY *)v10;
+    pWnfProcessContext->WnfProcessesListEntry.Blink = (_LIST_ENTRY *)v10;
     *v10 = p_WnfProcessesListEntry;
-    off_140D2C648 = (void **)&v7->WnfProcessesListEntry.Flink;
+    off_140D2C648 = (void **)&pWnfProcessContext->WnfProcessesListEntry.Flink;
+      
     if ( (_InterlockedExchangeAdd64((volatile signed __int64 *)&ExpWnfProcessesListLock, 0xFFFFFFFFFFFFFFFFui64) & 6) == 2 )
       ExfTryToWakePushLock(&ExpWnfProcessesListLock);
     KeAbPostRelease(&ExpWnfProcessesListLock);
